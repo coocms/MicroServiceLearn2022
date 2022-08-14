9642b952637f6534089c388d45fb841d8dccee62
coocms <623648048@qq.com>
2022-08-14 17:30:37 +0800

    Ocelot 自定义 负载均衡策略

---
+++ MicroServiceLearn/Microservice.OcelotGateway/OcelotExtend/LoadBanlanceExtend/CustomPollingLoadBalancer.cs
@@ -0,0 +1,42 @@
+﻿using Ocelot.LoadBalancer.LoadBalancers;
+using Ocelot.Responses;
+using Ocelot.Values;
+
+namespace Microservice.OcelotGateway.OcelotExtend.LoadBanlanceExtend
+{
+
+    public class CustomPollingLoadBalancer : ILoadBalancer
+    {
+        private readonly Func<Task<List<Service>>> _DownstreamServicesTaskList;
+        private readonly object CustomPollingLoadBalancer_Lock = new object();
+        private int _lastIndex;
+
+        public CustomPollingLoadBalancer(Func<Task<List<Service>>> services)
+        {
+            this._DownstreamServicesTaskList = services;
+        }
+
+        public async Task<Response<ServiceHostAndPort>> Lease(HttpContext httpContext)
+        {
+            var downstreamServices = await this._DownstreamServicesTaskList();
+            lock (CustomPollingLoadBalancer_Lock)
+            {
+                Console.WriteLine($"This is {nameof(CustomPollingLoadBalancer)}.Lease");
+                Console.WriteLine($"This is {httpContext.Request.Host.Value}");
+                Console.WriteLine($"This is {string.Join(",", downstreamServices.Select(s => s.HostAndPort.DownstreamHost + s.HostAndPort.DownstreamPort))}");
+                if (_lastIndex >= downstreamServices.Count)
+                {
+                    _lastIndex = 0;
+                }
+
+                var next = downstreamServices[_lastIndex];
+                _lastIndex++;
+                return new OkResponse<ServiceHostAndPort>(next.HostAndPort);
+            }
+        }
+
+        public void Release(ServiceHostAndPort hostAndPort)
+        {
+        }
+    }
+}
--- MicroServiceLearn/Microservice.OcelotGateway/Program.cs
+++ MicroServiceLearn/Microservice.OcelotGateway/Program.cs
@@ -1,3 +1,4 @@
+using Microservice.OcelotGateway.OcelotExtend.LoadBanlanceExtend;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -6,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 //builder.Services.AddControllersWithViews();
 builder.Configuration.AddJsonFile("ocelotconsul.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(); //×¢Èë
+builder.Services.AddOcelot().AddCustomLoadBalancer((serviceProvider, Route, serviceDiscoveryProvider) => new CustomPollingLoadBalancer(serviceDiscoveryProvider.Get)); //×¢Èë
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
