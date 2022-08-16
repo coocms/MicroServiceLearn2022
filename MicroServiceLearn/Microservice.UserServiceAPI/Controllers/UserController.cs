bc3a09f8771f4e5b234f749f83fa2f3caa3184d4
coocms <coocms66@gmail.com>
2022-08-16 18:06:09 +0800

    jwt userapi

---
+++ MicroServiceLearn/Microservice.UserServiceAPI/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microserivce.Interface;
+using Microservice.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.UserServiceAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        
+        private readonly IConfiguration _configuration;
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+
+        public UserController(IUserService userService, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
+        {
+            _userService = userService; 
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        [Authorize]//需要授权
+        [HttpGet]
+        public IActionResult GetUserAll()
+        {
+            Console.WriteLine($"This is UsersController {_configuration["port"] ?? _configuration["port"]} Invoke");
+            var host = _httpContextAccessor.HttpContext!.Request.Host;
+
+            return Ok(_userService.UserAll().Select(u => new User()
+            {
+                Id = u.Id,
+                Account = u.Account + "MA",
+                Name = u.Name,
+                Role = $"{_configuration["ip"] ?? host.Host}" +
+                $"{_configuration["port"] ?? (host.Port is null ? "NonePort" : host.Port!.Value.ToString())}",
+                Email = u.Email,
+                LoginTime = u.LoginTime,
+                Password = u.Password + "K8S"
+            }));
+        }
+
+
+
+    }
+}
---
+++ MicroServiceLearn/Microservice.UserServiceAPI/Program.cs
@@ -0,0 +1,63 @@
+using Microserivce.Interface;
+using Microservice.Framework;
+using Microservice.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();//如果需要获取HttpContext
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//#region JWT
+JWTTokenOptions tokenOptions = new JWTTokenOptions();
+builder.Configuration.Bind("JWTTokenOptions", tokenOptions);
+
+builder.Services
+.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)//Bearer Scheme
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        //JWT有一些默认的属性，就是给鉴权时就可以筛选了
+        ValidateIssuer = true,//是否验证Issuer
+        ValidateAudience = true,//是否验证Audience
+        ValidateLifetime = true,//是否验证失效时间---默认还添加了300s后才过期
+        ClockSkew = TimeSpan.FromSeconds(0),//token过期后立马过期
+        ValidateIssuerSigningKey = true,//是否验证SecurityKey
+
+        ValidAudience = tokenOptions.Audience,//Audience,需要跟前面签发jwt的设置一致
+        ValidIssuer = tokenOptions.Issuer,//Issuer，这两项和前面签发jwt的设置一致
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOptions.SecurityKey)),//拿到SecurityKey
+    };
+});
+
+//#endregion
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();//鉴权
+app.UseAuthorization();//授权
+
+app.MapControllers();
+
+app.Run(); 
